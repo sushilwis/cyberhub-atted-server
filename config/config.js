ee3c96ed5003f0f647e12128261d8bd94ccf4e57
sushil.wis <sushil.webideasolution@gmail.com>
2018-12-11 17:45:55 +0530

    first commit

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    localDB: "mongodb://localhost:27017/cyberhubAttendence",
+    // localDB: "mongodb://localhost:27017/cyberhubAttendence",
+    localDB: "mongodb://<sushil50889>:<mandi2232>@ds121182.mlab.com:21182/cyberhub",
 }
